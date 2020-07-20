7840df18eb4e408dedf8dab81bbcfd0120a2b3d2
Allison <Allison@Telmas-MBP.hsd1.wa.comcast.net>
2020-07-19 23:34:26 -0700

    test

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+//**this is a test 123
 
 function About(props) {
 
